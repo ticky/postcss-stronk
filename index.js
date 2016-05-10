82e426723aeebcc9843aa75e5212fa58ffa79869
Jessica Stokes <hello@jessicastokes.net>
2016-05-09 22:59:22 -0700

    Format tests ⭕️, DRY code off a bit 💦

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var postcss = require('postcss');
 
+var flexRegex = /💪(🏻|🏼|🏽|🏾|🏿)?/;
+
 module.exports = postcss.plugin('postcss-stronk', function () {
     return function (css) {
         css.walkDecls('display', function (decl) {
-            if (decl.value.match(/💪(🏻|🏼|🏽|🏾|🏿)?/)) {
+            if (decl.value.match(flexRegex)) {
                 decl.value = 'flex';
             }
         });
-        css.walkDecls(/💪(🏻|🏼|🏽|🏾|🏿)?/, function (decl) {
-            decl.prop = decl.prop.replace(/💪(🏻|🏼|🏽|🏾|🏿)?/, 'flex');
+        css.walkDecls(flexRegex, function (decl) {
+            decl.prop = decl.prop.replace(flexRegex, 'flex');
         });
     };
 });
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 import postcss from 'postcss';
-import test    from 'ava';
+import test from 'ava';
 
 import plugin from './';
 
@@ -12,35 +12,57 @@ function run(t, input, output, opts = { }) {
 }
 
 test('converts "💪" value to "flex"', t => {
-    return run(t, 'a{ display: 💪; }', 'a{ display: flex; }', { });
+    return run(t,
+        'a{ display: 💪; }',
+        'a{ display: flex; }'
+    );
 });
 
 test('converts "💪🏾" value to "flex"', t => {
-    return run(t, 'a{ display: 💪🏾; }', 'a{ display: flex; }', { });
+    return run(t,
+        'a{ display: 💪🏾; }',
+        'a{ display: flex; }'
+    );
 });
 
 test('converts "💪" prop to "flex"', t => {
-    return run(t, 'a{ 💪: 2; }', 'a{ flex: 2; }', { });
+    return run(t,
+        'a{ 💪: 2; }',
+        'a{ flex: 2; }'
+    );
 });
 
 test('converts "💪🏻" prop to "flex"', t => {
-    return run(t, 'a{ 💪🏻: 2; }', 'a{ flex: 2; }', { });
+    return run(t,
+        'a{ 💪🏻: 2; }',
+        'a{ flex: 2; }'
+    );
 });
 
 test('converts "💪-grow" prop to "flex-grow"', t => {
-    return run(t, 'a{ 💪-grow: 0.6; }', 'a{ flex-grow: 0.6; }', { });
+    return run(t,
+        'a{ 💪-grow: 0.6; }',
+        'a{ flex-grow: 0.6; }'
+    );
 });
 
 test('converts "💪🏿-grow" prop to "flex-grow"', t => {
-    return run(t, 'a{ 💪🏿-grow: 0.6; }', 'a{ flex-grow: 0.6; }', { });
+    return run(t,
+        'a{ 💪🏿-grow: 0.6; }',
+        'a{ flex-grow: 0.6; }'
+    );
+});
+
+test('converts "-ms-💪-order" prop to "-ms-flex-order"', t => {
+    return run(t,
+        'a{ -ms-💪-order: 2; }',
+        'a{ -ms-flex-order: 2; }'
+    );
 });
 
 test('won\'t break stronk content', t => {
     return run(t,
         'a::before{ content: \'💪\'; }',
-        'a::before{ content: \'💪\'; }');
-});
-
-test('converts "-ms-💪-order" prop to "-ms-flex-order"', t => {
-    return run(t, 'a{ -ms-💪-order: 2; }', 'a{ -ms-flex-order: 2; }', { });
+        'a::before{ content: \'💪\'; }'
+    );
 });
